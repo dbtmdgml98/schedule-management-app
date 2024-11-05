1c285adb737f6040d3a5689821dc59399be22bc9
dbtmdgml98 <dbtmdgml98@naver.com>
2024-11-06 01:18:32 +0900

    Feat: 일정을 CRUD 할 수 있는 Web Application 생성
    일정 생성 및 조회 기능에 상태 반환 코드 추가

--- src/main/java/com/example/schedulemanagementapp/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagementapp/controller/ScheduleController.java
@@ -3,6 +3,8 @@ package com.example.schedulemanagementapp.controller;
 import com.example.schedulemanagementapp.dto.ScheduleRequestDto;
 import com.example.schedulemanagementapp.dto.ScheduleResponseDto;
 import com.example.schedulemanagementapp.entity.Schedule;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,7 +27,7 @@ public class ScheduleController {
 
     // 일정 생성
     @PostMapping
-    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto dto) {
+    public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto) {
 
         // 식별자가 1씩 증가하도록 만듦
         Long scheduleId = scheduleList.isEmpty() ? 1 : Collections.max(scheduleList.keySet()) + 1;
@@ -34,16 +38,39 @@ public class ScheduleController {
         // Inmemory DB에 Schedule 저장
         scheduleList.put(scheduleId, schedule);
 
-        return new ScheduleResponseDto(schedule);
+        return new ResponseEntity<>(new ScheduleResponseDto(schedule), HttpStatus.CREATED);
     }
 
-    // 일정 조회
+    // 전체 일정 조회
+    @GetMapping
+    public ResponseEntity<List<ScheduleResponseDto>> findAllSchedules() {
+
+        // init List
+        List<ScheduleResponseDto> responseList = new ArrayList<>();
+
+        // HashMap(Schedule) -> List<ScheduleResponseDto>
+        for (Schedule schedule : scheduleList.values()) {
+            ScheduleResponseDto responseDto = new ScheduleResponseDto(schedule);
+            responseList.add(responseDto);
+        }
+
+        // Map to List
+//        responseList = scheduleList.values().stream().map (ScheduleResponseDto::new).toList();
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
+    }
+
+    // 선택 일정 조회
     @GetMapping("/{id}")
-    public  ScheduleResponseDto findScheduleById(@PathVariable Long id) {
+    public ResponseEntity<ScheduleResponseDto> findScheduleById(@PathVariable Long id) {
 
         Schedule schedule = scheduleList.get(id);
 
-        return new ScheduleResponseDto(schedule);
+        if (schedule == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new ScheduleResponseDto(schedule), HttpStatus.OK);
     }
 
 }
